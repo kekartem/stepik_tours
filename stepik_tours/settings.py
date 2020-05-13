baa013ad47680d945ec775b06e4b01ee34ba8d92
Artyom <arthack00@gmail.com>
2020-05-13 19:15:36 +0300

    changed some setting again

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dnlbp*4y4-g(xza^k21%*8-w-axa3189&be(hujzfd%5#(acu#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -21,6 +21,6 @@ from tours.views import MainView, DepartureView, TourView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainView.as_view()),
-    path('/departure/<str:departure>', DepartureView.as_view()),
-    path('/tour/<int:id>/', TourView.as_view()),
+    path('departure/<str:departure>/', DepartureView.as_view()),
+    path('tour/<int:tour_id>/', TourView.as_view()),
 ]
